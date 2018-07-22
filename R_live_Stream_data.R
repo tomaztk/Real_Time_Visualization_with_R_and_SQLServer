da519a5cf70246c78836be46f80f8c06f192b4e3
tomaztk <tomaz.kastrun@gmail.com>
2018-07-22 20:57:58 +0200

    Add files via upload

--- R_live_Stream_data.R
+++ R_live_Stream_data.R
@@ -5,9 +5,6 @@
 #Visualizing real-time data stream  
 #using R (and environments) and Microsoft SQL Server
 ########################################################
-
-library(ggplot2)
-library(gridExtra)
 library(RODBC)
 
 
@@ -17,21 +14,20 @@ assign("getREnvironment", new.env(), envir = .GlobalEnv)
 # Function to read data from SQL Server
 getSQLServerData <- function()
 {
+  #extract environment settings for storing data
   getREnvironment <- get("getREnvironment", envir = .GlobalEnv, mode = "environment")
-  
+  #get the SQL Server data
   con <- odbcDriverConnect('driver={SQL Server};server=TOMAZK\\MSSQLSERVER2017;database=test;trusted_connection=true')
   db_df <- sqlQuery(con, 'select TOP 20 id, num from LiveStatsFromSQLServer ORDER BY id DESC')
   close(con)
-    
-  #append any new data
-  dfCurr <- db_df
-  #getREnvironment$db_df <- rbind.data.frame(getREnvironment$db_df, dfCurr)
-  getREnvironment$db_df <- data.frame(dfCurr)
+  #overwrite existing data with new data
+  df_overwrite <- db_df
+  getREnvironment$db_df <- data.frame(df_overwrite)
   try(assign("getREnvironment", getREnvironment, envir = .GlobalEnv))
   invisible()  
 }
 
-
+# Plot graph 
 n=1000 #nof iterations
 windowQuery=20 # syncronised with TOP clause in SELECT statement
 for (i in 1:(n-windowQuery)) {
@@ -39,10 +35,6 @@ for (i in 1:(n-windowQuery)) {
   getSQLServerData()
   getREnvironment <- get("getREnvironment", envir = .GlobalEnv, mode = "environment")
   data <- getREnvironment$db_df
-  plot(data$id,data$num,type='l', main='Realtime data from SQL Server')
-  #ggplot(data=data, aes(x=data$id, y=data$num)) +geom_line()
+  plot( data$id, data$num, type='l',main='Realtime data from SQL Server')
   Sys.sleep(0.5)
 }
-
-
-
